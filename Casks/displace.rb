bb31f93ef15e70a070f46f742492e5e86a77ba5f
JereSalo <jeresalo17@gmail.com>
2026-06-25 23:12:32 -0300

    displace 2.0.1

--- Casks/displace.rb
+++ Casks/displace.rb
@@ -1,6 +1,6 @@
 cask "displace" do
-  version "2.0.0"
-  sha256 "a4934b974c4c5f13dd020a0ff4925e874a8a8090f126df33ac69a2ae6cdbd121"
+  version "2.0.1"
+  sha256 "a1c600c305e1b044bb2e3ece2f74df93681c4a4fc483b62ec9aa4478a0fb2f37"
 
   url "https://api.displaceapp.com/download/#{version}",
       verified: "api.displaceapp.com/"
